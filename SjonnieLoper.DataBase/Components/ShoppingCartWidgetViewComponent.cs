2f4a8f7bd32370f5086443ac08f01c6f2dd9f7c6
Stella van der burg <estellavanderburg@gmail.com>
2020-11-13 13:29:25 +0100

    - Added CartWidget ViewComponent - Removed ShoppingCart Link from Layout, replaced it with Widget.

---
+++ SjonnieLoper.DataBase/Components/ShoppingCartWidgetViewComponent.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SjonnieLoper.DataBase.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SjonnieLoper.DataBase.Components
+{
+    public class ShoppingCartWidgetViewComponent : ViewComponent
+    {
+        private readonly ShoppingCart _shoppingCart;
+
+        public ShoppingCartWidgetViewComponent(ShoppingCart shoppingCart)
+        {
+            _shoppingCart = shoppingCart;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var items = await _shoppingCart.GetCartItemsAsync();
+            _shoppingCart.ShoppingCartItems = items;
+
+            return View(items);
+        }
+    }
+}
